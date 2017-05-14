db6301a4c390b7acf0819b4c91ed618806beedcb
Anas Harby <anas.harby@gmail.com>
2017-05-14 20:35:57 +0200

    Minor changes in UI

--- Gui.py
+++ Gui.py
@@ -1,17 +1,23 @@
 import sys
 
-from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.uic import loadUi
 
 
-class DemoImpl(QMainWindow):
+class EquationSolverUi(QMainWindow):
     def __init__(self, *args):
-        super(DemoImpl, self).__init__(*args)
+        super(EquationSolverUi, self).__init__(*args)
         loadUi('part1.ui', self)
+        self.solve_btn.clicked.connect(self.solve_eq)
+
+    @QtCore.pyqtSlot()
+    def solve_eq(self):
+        print("SOLVING")
 
 
-app = QApplication(sys.argv)
-widget = DemoImpl()
-widget.show()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = EquationSolverUi()
+    widget.show()
+    sys.exit(app.exec_())
